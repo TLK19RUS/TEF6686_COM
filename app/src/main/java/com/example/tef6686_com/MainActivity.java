0b084a2456ad946cee4a4727c3fb1681581e2c73
User <tlk19rus@gmail.com>
2021-03-29 23:03:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tef6686_com/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.tef6686_com;
+
+import android.content.Context;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity  {
+    private static UsbSerialPort sPort = null;
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+        if (!availableDrivers.isEmpty()) {
+            EditText log = (EditText) this.findViewById(R.id.editTextTextMultiLine);
+            for (UsbSerialDriver drv :availableDrivers
+                 ) {
+                log.append(drv.getDevice().getProductName()+ "\r\n");
+            }
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
